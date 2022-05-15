23bb99ac123d2d99278afd833b7d732421608d1c
pulchroxloom <code@dantebarbieri.dev>
2022-05-14 21:34:51 -0500

    Fix image resize bug

--- src/components/low-level/PixelatedImage.tsx
+++ src/components/low-level/PixelatedImage.tsx
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box/Box'
 import useTheme from '@mui/material/styles/useTheme';
 import Art from '../../utils/interfaces/art';
 import CircularProgress from '@mui/material/CircularProgress/CircularProgress';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { BoxTypeMap } from '@mui/system/Box/Box';
 
 type Props = {
     art: Art,
@@ -16,6 +18,7 @@ const PixelatedImage = (props: Props) => {
 
     const canvasRef = React.useRef<HTMLCanvasElement>(null)
     const imgRef = React.useRef<HTMLImageElement>(null)
+    const boxRef = React.useRef<OverridableComponent<BoxTypeMap<{}, "div">>>(null);
 
     const resizeCanvas = React.useCallback(() => {
         const canvas = canvasRef.current
@@ -24,6 +27,11 @@ const PixelatedImage = (props: Props) => {
             canvas.width = img.naturalWidth * canvas.clientHeight / img.naturalHeight
         }
     }, [])
+
+    const resizeImage = React.useCallback(() => {
+        imgRef.current?.setAttribute('max-height', `${(boxRef.current as unknown as HTMLDivElement | undefined)?.clientHeight}`)
+        console.log('max-height', `${(boxRef.current as unknown as HTMLDivElement | undefined)?.clientHeight}`)
+    }, [boxRef])
 
     const redrawCanvas = React.useCallback(() => {
         const canvas = canvasRef.current
@@ -43,7 +51,7 @@ const PixelatedImage = (props: Props) => {
         }
     }, [props.pixelation])
 
-    const repaintCanvas = React.useCallback(() => { resizeCanvas(); redrawCanvas() }, [resizeCanvas, redrawCanvas])
+    const repaintCanvas = React.useCallback(() => { resizeCanvas(); resizeImage(); redrawCanvas() }, [resizeCanvas, resizeImage, redrawCanvas])
 
     const setupCallback = React.useCallback(() => { setLoading(false) }, [])
 
@@ -71,7 +79,7 @@ const PixelatedImage = (props: Props) => {
             }}>
                 <CircularProgress size={theme.spacing(10)} />
             </Box>)}
-            <Box sx={{
+            <Box ref={boxRef} sx={{
                 display: loading ? 'none' : 'flex',
                 flexDirection: 'column',
                 bgcolor: theme.palette.background.default,
@@ -79,9 +87,12 @@ const PixelatedImage = (props: Props) => {
                 border: `solid ${theme.spacing(1)} currentColor`,
                 borderRadius: theme.spacing(0.5),
                 m: 4,
-                flexGrow: 1
+                flexGrow: props.pixelation
             }}>
-                <img ref={imgRef} src={props.art.url} alt={props.art.title} height={canvasRef.current?.clientHeight} style={{ display: loading ? 'none' : props.pixelation !== 0 ? 'none' : 'initial' }} />
+                <img ref={imgRef} src={props.art.url} alt={props.art.title} style={{
+                    maxWidth: '100%',
+                    maxHeight: (boxRef.current as unknown as HTMLDivElement | undefined)?.clientHeight,
+                    display: loading ? 'none' : props.pixelation !== 0 ? 'none' : 'initial' }} />
                 <canvas ref={canvasRef} style={{ flexGrow: 1, display: loading ? 'none' : props.pixelation === 0 ? 'none' : 'initial' }} />
             </Box>
         </>
